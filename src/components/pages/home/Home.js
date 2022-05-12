41b2465e7923c7754ea3d69205daef695037f899
Damian <damiszalai@gmail.com>
2022-05-12 14:00:22 -0300

    cambios en el hero

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -10,12 +10,14 @@ const Home = () => {
     <Container className="container-fluid">
       <Slider />
       <div className="subHero">
+        <div className="container">
         <p>
           ¡VEN A MEJORAR TU RENDIMIENTO PARA OBTENER UNA BECA DEPORTIVA EN LAS
           MEJORES UNIVERSIDADES DE USA, CHILE, O PORQUÉ NO, LLEGAR A SER UN
           DEPORTISTA PROFESIONAL!
         </p>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb56zbIvuryesCU4q7xHTgc2VB6oSigFvKfyo7mau5kqlLIg/viewform" className="btn">Postulá aquí</a>
+        </div>
       </div>
       <Counter />
       <Gallery />
--- src/components/pages/home/style.js
+++ src/components/pages/home/style.js
@@ -1,35 +1,49 @@
 import styled from "styled-components";
+import { devices } from "../../UI/atoms/devices";
 
 const Container = styled.div`
   padding: 0;
   background: #212529;
   .subHero {
     background: #ffff00;
-    padding: 24px;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    .container {
+      padding: 24px;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
 
-    p {
-      font-size: 1.4em;
-      font-weight: bold;
-      font-style: italic;
-      line-height: 1;
-      width: 700px;
-      text-align:left;
-      margin: 0 120px 0 0;
-    }
-    a {
-      background: white;
-      color: black;
-      margin-top: 24px;
-      font-weight: bold;
-      padding: 0 24px;
-      height: 45px;
-      line-height: 45px;
-      text-transform: uppercase;
-      margin: 0;
+      @media ${devices.laptop} {
+        flex-direction: row;
+        justify-content: space-between;
+      }
+
+      p {
+        font-size: 1.4em;
+        font-weight: bold;
+        font-style: italic;
+        line-height: 1;
+        width: 700px;
+        text-align: center;
+        margin: 0 0px 12px 0;
+        max-width: 100%;
+        @media ${devices.laptop} {
+          margin-right: 120px;
+          text-align: left;
+        }
+      }
+      a {
+        background: white;
+        color: black;
+        margin-top: 24px;
+        font-weight: bold;
+        padding: 0 24px;
+        height: 45px;
+        line-height: 45px;
+        text-transform: uppercase;
+        margin: 0;
+      }
     }
   }
 `;
