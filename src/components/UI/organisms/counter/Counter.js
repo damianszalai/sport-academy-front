4d0be7281376f65114c9e940a405991da6db829c
Damian <damiszalai@gmail.com>
2022-05-12 19:02:16 -0300

    cambios en el hero

--- src/components/UI/organisms/counter/Counter.js
+++ src/components/UI/organisms/counter/Counter.js
@@ -42,7 +42,7 @@ const Counter = () => {
       </div>
       <div>
         <iframe
-          width="560"
+          /* width="560" */
           height="315"
           src="https://www.youtube.com/embed/V6iLWQB6hT8"
           title="YouTube video player"
