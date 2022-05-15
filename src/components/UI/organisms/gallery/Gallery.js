c59077a1707726bd0dcfcb56a1407705b3eb28fc
Damian <damiszalai@gmail.com>
2022-05-14 21:40:36 -0300

    cambios en el hero

--- src/components/UI/organisms/gallery/Gallery.js
+++ src/components/UI/organisms/gallery/Gallery.js
@@ -1,46 +1,48 @@
 import React, { useState } from "react";
 import Container from "./style";
+import Imagen from "./Imagen";
 import { motion } from "framer-motion";
 import { aaMin, bbMin, ccMin } from "../../../../assets";
 
 const Gallery = () => {
   const [image, setImage] = useState(null);
 
-  const imageNes = [aaMin, bbMin, ccMin, aaMin, bbMin, ccMin, aaMin, bbMin, ccMin];
+  const imageNes = [
+    aaMin,
+    bbMin,
+    ccMin,
+    aaMin,
+    bbMin,
+    ccMin,
+    aaMin,
+    bbMin,
+    ccMin,
+  ];
 
   return (
     <Container>
       <figure className="container">
-        {imageNes.map((item, i) => (
+        {
+          imageNes.map((item, i) => (
           <motion.img
             initial={{ transform: "scale(0)", opacity: 0 }}
             animate={{ transform: "scale(1)", opacity: 1 }}
             transition={{
               duration: 0.45,
-              ease: [0.96, -0.23, 0.03, 1.27],
+              ease: [0.96, -0.23, 0.03, 1.27]
             }}
             key={i}
             alt="i"
             src={item}
             onClick={() => setImage(item)}
-          ></motion.img>
-        ))}
+          >
+            
+          </motion.img>
+        ))
+        }
       </figure>
-      {image && (
-        <div onClick={() => setImage(null)}>
-         
-          <i class="bi bi-x-lg" onClick={() => setImage(null)}></i>
-          <motion.img
-            src={image}
-            initial={{ transform: "translate(-50%,-70%)", opacity: 0 }}
-            animate={{ transform: "translate(-50%,-50%)", opacity: 1 }} 
-            transition={{
-              duration: 0.45,
-            }}
-          />
-        </div>
-      )}
-    </Container>
+      {image && <Imagen setImage={setImage} image={image} />}
+   </Container>
   );
 };
 
--- src/components/UI/organisms/gallery/Imagen.js
+++ src/components/UI/organisms/gallery/Imagen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-const Imagen = () => {
+const Imagen = ({image, setImage}) => {
   return (
-    <div>Imagen</div>
-  )
-}
+    <div onClick={() => setImage(null)}>
+      <i class="bi bi-x" onClick={() => setImage(null)}></i>
+      <motion.img
+        src={image}
+        initial={{ transform: "translate(-50%,-70%)", opacity: 0 }}
+        animate={{ transform: "translate(-50%,-50%)", opacity: 1 }}
+        transition={{
+          duration: 0.45,
+        }}
+      />
+    </div>
+  );
+};
 
-export default Imagen
+export default Imagen;
